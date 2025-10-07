1a55375d6e3f13b77ac7656cbf1643d6fbcf9369
bartlomiejsitek <bartlomiej.sitek0@gmail.com>
2025-10-07 09:24:11 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-(function () {
-  //TODO
+(function() {
+  var ex1_button = document.getElementById('ex1_button');
+  ex1_button.onclick = function() {
+    var numbers = [];
+    for (var i = 0; i <= 9; i++) {
+      numbers.push(i);
+    }
+    var ex1_content = document.getElementById('ex1_content');
+    ex1_content.innerHTML = numbers.join(', ');
+  }
 })();
