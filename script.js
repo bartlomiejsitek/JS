ce5b99340f8d1d21f151db6c50fdb7395c0a0d78
bartlomiejsitek <bartlomiej.sitek0@gmail.com>
2025-10-07 09:45:42 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -8,4 +8,19 @@
     var ex1_content = document.getElementById('ex1_content');
     ex1_content.innerHTML = numbers.join(', ');
   }
+
+
+  var ex2_text = document.getElementById('ex2_text');
+  ex2_text.oninput = function() {
+    var ex2_content = document.getElementById('ex2_content');
+    var value = ex2_text.value;
+
+    if (value.length !== 9) {
+      ex2_content.innerHTML = 'dlugosc musi byc rowna 9';
+    } else if (!/^\d+$/.test(value)) {
+      ex2_content.innerHTML = 'zawiera litery lub znaki specjalne';
+    } else {
+      ex2_content.innerHTML = 'numer telefonu jest poprawny';
+    }
+  }
 })();
